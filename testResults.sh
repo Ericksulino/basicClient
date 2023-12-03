94d1badaaf49483aed6615c82162bea2e4be0335
ericksulino <ericksulino@gmail.com>
2023-12-03 11:57:03 -0300

    update sh

--- testResults.sh
+++ testResults.sh
@@ -1,4 +1,5 @@
+echo "StartTime Hash EndorseTime CommitTime TotalTime" > teste20x100.csv
 for i in {1..20}; do
-  (sudo node dist/client.js createAssetEndorse 100 B> 20) &
-done
+  (sudo node dist/client.js createAssetEndorse 100 B) &
+done >> teste.csv
 wait
